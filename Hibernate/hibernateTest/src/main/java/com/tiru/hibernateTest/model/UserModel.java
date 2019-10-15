99c748cb3c2e8bd3c21a9566da664d4601cf1164
TK00341155 <TK00341155@INMUCHLP03028.TechMahindra.com>
2019-10-15 13:36:13 +0200

    Updated User Model

---
+++ Hibernate/hibernateTest/src/main/java/com/tiru/hibernateTest/model/UserModel.java
@@ -0,0 +1,20 @@
+package com.tiru.hibernateTest.model;
+
+public class UserModel {
+	
+	private String userName;
+	private int userID;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	
+}
