ce8adf9f44a731beb13a7d6c8528019316bc4eaa
Tiruvenkatesh Konar <venky.rocks.03@gmail.com>
2019-10-15 11:30:49 +0200

    First COmmit

---
+++ Hibernate/hibernateTest/src/main/java/com/tiru/hibernateTest/App.java
@@ -0,0 +1,13 @@
+package com.tiru.hibernateTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
